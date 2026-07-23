08eb81857077fe8eef4563b8f1fc97ad0390a12a
Asif Rabetul <asifrabetul@gmail.com>
2026-07-23 21:42:55 +0600

    Remove dark mode toggle button from topbar header in app layout

--- app/views/layouts/app.php
+++ app/views/layouts/app.php
@@ -127,11 +127,6 @@
 
             <!-- Header Actions -->
             <div class="topbar-actions">
-                <!-- Theme Switcher Trigger Button -->
-                <button class="btn btn-ghost btn-icon" data-theme-toggle>
-                    <svg class="dark-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-                </button>
-
                 <!-- Notifications Bell -->
                 <button class="btn btn-ghost btn-icon" style="position: relative;">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>
